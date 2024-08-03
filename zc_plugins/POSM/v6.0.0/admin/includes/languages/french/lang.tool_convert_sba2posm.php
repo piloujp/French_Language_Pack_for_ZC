7b06bf76dd681ca29ca9e4bb8ccdefd4fe76e3df
piloujp <117885587+piloujp@users.noreply.github.com>
2024-08-04 01:24:14 +0900

    Added missing escape characters

--- zc_plugins/POSM/v6.0.0/admin/includes/languages/french/lang.tool_convert_sba2posm.php
+++ zc_plugins/POSM/v6.0.0/admin/includes/languages/french/lang.tool_convert_sba2posm.php
@@ -6,7 +6,7 @@
 $define = [
     'HEADING_TITLE' => 'Stock des options de produits :  Convertir Stock par attributs (Stock-by-Attribute)',
 
-    'TEXT_INSTRUCTIONS' => 'Utilisez cet outil pour convertir vos tables de base de données <em>Stock par attributs (SBA)</em> existantes en tables de base de données <em>Stock d\'options de produits (POSM)</em> associées. Veuillez noter que <em>POSM</em> nécessite que toutes les combinaisons d\'options soient spécifiées, donc certaines de vos combinaisons <em>SBA</em> peuvent ne pas être « convertibles ». Reportez-vous à l'affichage ci-dessous ; si la colonne d'état affiche des options manquantes ou un produit inconnu, cet enregistrement <em>SBA</em> ne sera pas converti !<br /><br /><strong>Note :</strong> Cliquer sur le bouton « Envoyer » supprimera toutes les entrées existantes dans votre configuration POSM !',
+    'TEXT_INSTRUCTIONS' => 'Utilisez cet outil pour convertir vos tables de base de données <em>Stock par attributs (SBA)</em> existantes en tables de base de données <em>Stock d\'options de produits (POSM)</em> associées. Veuillez noter que <em>POSM</em> nécessite que toutes les combinaisons d\'options soient spécifiées, donc certaines de vos combinaisons <em>SBA</em> peuvent ne pas être « convertibles ». Reportez-vous à l\'affichage ci-dessous ; si la colonne d\'état affiche des options manquantes ou un produit inconnu, cet enregistrement <em>SBA</em> ne sera pas converti !<br /><br /><strong>Note :</strong> Cliquer sur le bouton « Envoyer » supprimera toutes les entrées existantes dans votre configuration POSM !',
 
     'ERROR_NO_SBA_TABLE' => 'Aucune conversion n\'est possible &mdash; table de base de données <em>products_with_attributes_stock</em> manquante.',
 
@@ -14,7 +14,7 @@ $define = [
     'BUTTON_ALT_TEXT' => 'Cliquez ici pour convertir les tables',
 
     'TEXT_MISSING_OPTIONS' => '<span class="missing">&cross; Options (%s) manquantes</span>',
-    'TEXT_UNSUPPORTED_OPTION_TYPE' => '<span class="missing">&cross; L'ID d'option (%1$u) utilise un type d\'option (%2$u) non pris en charge</span>',
+    'TEXT_UNSUPPORTED_OPTION_TYPE' => '<span class="missing">&cross; L\'ID d\'option (%1$u) utilise un type d\'option (%2$u) non pris en charge</span>',
     'TEXT_MISSING_PRODUCT' => '<span class="missing">&cross; Le produit n\'existe pas</span>',
     'TEXT_OK' => '<span class="ok">&check;</span>',
 
