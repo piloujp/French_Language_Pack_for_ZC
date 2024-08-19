f410e89fbff7aab10f1971e776b3add560791c86
piloujp <117885587+piloujp@users.noreply.github.com>
2024-08-19 16:32:51 +0900

    Boxes layout language files update

--- admin/includes/languages/french/lang.layout_controller.php
+++ admin/includes/languages/french/lang.layout_controller.php
@@ -1,57 +1,72 @@
 <?php
 /**
- * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Scott C Wilson 2022 Jan 11 New in v1.5.8-alpha $
+ * @version $Id: DrByte 2024 May 27 Modified in v2.1.0-alpha1 $
 */
 
 $define = [
-    'HEADING_TITLE' => 'Modification des blocs latéraux pour le modèle : ',
+    'HEADING_TITLE' => 'Modification des cases latérales pour le modèle : ',
     'TEXT_CURRENTLY_VIEWING' => 'Actuellement consulté : ',
     'TEXT_THIS_IS_PRIMARY_TEMPLATE' => ' (Principal)',
-    'TABLE_HEADING_LAYOUT_BOX_NAME' => 'Nom du Bloc',
-    'TABLE_HEADING_LAYOUT_BOX_STATUS' => 'Statut dans<br>la COLONNE<br>GAUCHE/DROITE',
-    'TABLE_HEADING_LAYOUT_BOX_STATUS_SINGLE' => 'Statut dans<br>la COLONNE<br>UNIQUE',
-    'TABLE_HEADING_LAYOUT_BOX_LOCATION' => 'COLONNE<br>GAUCHE ou DROITE',
-    'TABLE_HEADING_LAYOUT_BOX_SORT_ORDER' => 'Classement<br>dans la COLONNE<br>GAUCHE/DROITE',
-    'TABLE_HEADING_LAYOUT_BOX_SORT_ORDER_SINGLE' => 'Classement<br>dans la COLONNE<br>UNIQUE',
-    'TEXT_INFO_LAYOUT_BOX' => 'Bloc sélectionné: ',
-    'TEXT_INFO_LAYOUT_BOX_NAME' => 'Nom du bloc:',
-    'TEXT_INFO_LAYOUT_BOX_LOCATION' => 'Emplacement : ',
-    'TEXT_INFO_LAYOUT_BOX_STATUS' => 'Statut dans la colonne gauche/droite: ',
-    'TEXT_INFO_LAYOUT_BOX_STATUS_SINGLE' => 'Statut dans la colonne unique: ',
-    'TEXT_INFO_LAYOUT_BOX_SORT_ORDER' => 'Classement dans la colonne gauche/droite:',
-    'TEXT_INFO_LAYOUT_BOX_SORT_ORDER_SINGLE' => 'Classement dans la colonne unique:',
-    'TEXT_INFO_INSERT_INTRO' => 'Veuillez saisir le nouveau bloc avec ses données relatives',
-    'TEXT_INFO_DELETE_INTRO' => 'Êtes-vous certain(e) de vouloir supprimer ce bloc ?',
-    'TEXT_INFO_HEADING_EDIT_BOX' => 'Éditer le bloc',
-    'TEXT_INFO_HEADING_DELETE_BOX' => 'Supprimer le bloc',
-    'TEXT_INFO_DELETE_MISSING_LAYOUT_BOX' => 'Supprimer le bloc manquant de la liste des modèles : ',
-    'TEXT_INFO_DELETE_MISSING_LAYOUT_BOX_NOTE' => 'NOTE: Cette opération ne supprime pas de fichiers et vous pouvez réinstaller le bloc à tout moment en l\'ajoutant au répertoire correspondant.<br><br><strong>Effacer le nom du bloc: </strong>',
-    'TEXT_INFO_RESET_TEMPLATE_SORT_ORDER' => 'Réinitialiser le classement de tous les blocs au classement PAR DÉFAUT des blocs du template: ',
-    'TEXT_INFO_RESET_TEMPLATE_SORT_ORDER_NOTE' => 'Cette opération ne supprime aucun bloc. Elle réinitialise seulement le classement actuel.',
-    'TEXT_SETTINGS_COPY_FROM' => 'Copier les paramètres d\'état/de tri DEPUIS : ',
-    'TEXT_SETTINGS_COPY_TO' => ' VERS : ',
-    'TEXT_ERROR_INVALID_RESET_SUBMISSION' => 'ERREUR : Choix de réinitialisation non valide',
-    'TEXT_INFO_BOX_DETAILS' => 'Détails du bloc: ',
-    'TABLE_HEADING_LAYOUT_TITLE' => 'Intitulé',
-    'TABLE_HEADING_LAYOUT_VALUE' => 'Valeur',
-    'TABLE_HEADING_BOXES_PATH' => 'Chemin d\'accès des blocs',
-    'TEXT_WARNING_NEW_BOXES_FOUND' => 'AVERTISSEMENT : Nouveaux blocs trouvés : ',
-    'TEXT_MODULE_DIRECTORY' => 'Répertoire de disposition du site : ',
-    'TEXT_GOOD_BOX' => ' ',
-    'TEXT_BAD_BOX' => '<span style="color:#ff0000;"><b>MANQUANT</b></span><br>',
-    'SUCCESS_BOX_DELETED' => 'Le bloc a été supprimé : ',
+    'TABLE_HEADING_BOXES_PATH' => 'Chemin des cases : ',
+    'TEXT_WARNING_NEW_BOXES_FOUND' => 'AVERTISSEMENT : Nouvelles cases trouvées : ',
+    'TEXT_ORIGINAL_DEFAULTS' => '[Paramètres par défaut d\'origine/principaux de Zen-Cart',
+    'TEXT_CAUTION_EDITING_NOT_LIVE_TEMPLATE' => 'CAUTION: Vous modifiez les paramètres d\'un modèle qui n\'est pas le modèle principal utilisé par les clients.',
+
+    'TEXT_HEADING_MISSING_BOXES' => 'Cases manquantes',
+    'BUTTON_REMOVE_SELECTED' => 'Supprimer la sélection',
+    'TEXT_NO_BOXES_TO_REMOVE' => 'Aucune case latérale manquante n\'a été sélectionnée pour être supprimée.',
+    'BUTTON_REMOVE_BOXES' => 'Supprimer les cases',
+    'BUTTON_CLOSE' => 'Fermer',
+
+    'TEXT_INFO_HEADING_DELETE_BOX' => 'Supprimer les cases latérales manquantes',
+    'TEXT_INFO_DELETE_MISSING_LAYOUT_BOX_NOTE' => 'REMARQUE : Cela ne supprime pas les fichiers et vous pouvez rajouter les cases à tout moment en les ajoutant au bon répertoire.<br><br><strong>Cases à supprimer : </strong>',
+    'SUCCESS_BOX_DELETED' => 'Ces cases ont été supprimées : ',
+
+    'TEXT_RESET_SETTINGS' => 'Réinitialiser les paramètres',
+    'TEXT_INFO_RESET_TEMPLATE_SORT_ORDER' => 'Réinitialiser l\'état de la case/paramètres de tri : ',
+    'TEXT_INFO_RESET_TEMPLATE_SORT_ORDER_NOTE' => 'Cela ne supprime aucune case. Cela réinitialisera uniquement le statut/l\'ordre de tri des cases correspondantes aux cases de l\'autre modèle.',
+    'TEXT_SETTINGS_COPY_FROM' => 'Copier l\'état/les paramètres de tri DE : ',
+    'TEXT_SETTINGS_COPY_TO' => ' À : ',
     'SUCCESS_BOX_RESET' => 'Les paramètres de [%1$s] ont été réinitialisés aux paramètres actuels de [%2$s].',
+    'TEXT_ERROR_INVALID_RESET_SUBMISSION' => 'ERREUR : Choix de réinitialisation non valide',
+
+    'TEXT_INSTRUCTIONS' => 'Si votre appareil est équipé d\'une souris, vous pouvez faire glisser et déposer une case pour modifier son emplacement de colonne ou son ordre de tri dans ses emplacements de colonne actifs. Sinon, utilisez les icônes de flèche vers le haut et vers le bas pour modifier l\'emplacement ou l\'ordre de tri d\'une case. Utilisez une icône <i class="fa-solid fa-xmark"></i> pour déplacer rapidement une case active vers son groupe inactif.',
+    'BUTTON_SHOW_NOTES' => 'Afficher les remarques :',
+    'BUTTON_HIDE_NOTES' => 'Masquer les remarques :',
+    'TEXT_NOTES' => 'Remarques :',
+    'TEXT_NOTE1_OPT' => 'L\'affichage de ces (%1$s) cases sur la vitrine dépend fortement du modèle %2$s. Vérifiez auprès de l\'auteur du modèle pour plus de détails !',
+    'TEXT_NOTE1' => 'Une fois que vous avez déplacé une case, un bouton s\'affiche qui, une fois cliqué, enregistre toutes les modifications que vous avez apportées.',
+    'TEXT_NOTE2' => 'Les ordres de tri des cases sont calculés lorsque vous enregistrez vos choix, pas besoin de les fournir.',
+    'TEXT_NOTE3' => 'Toutes les cases inactives sont enregistrées avec le même ordre de tri, afin qu\'elles s\'affichent par ordre alphabétique.',
+    'TEXT_NOTE4' => 'Déplacer une case dans un groupe inactif n’est pas considéré comme un changement !',
+    'TEXT_NOTE5' => 'Si vous avez apporté des modifications et que vous quittez cet outil, votre navigateur vous informera que vous avez des modifications non enregistrées.',
+
+    'TEXT_COLUMN_DISABLED' => 'Colonne désactivée globalement',
+    'TEXT_DISABLED_MESSAGE' => 'Les modifications apportées à cette colonne seront enregistrées, mais ne seront pas affichées sur la vitrine. Voir le paramètre associé dans Configuration :: Paramètres de mise en page.',
+    'TEXT_HEADING_MAIN_PAGE_BOXES' => 'Cases de la page principale',
+    'TEXT_HEADING_ACTIVE_LEFT' => 'Cases actives de la colonne de gauche',
+    'TEXT_HEADING_ACTIVE_RIGHT' => 'Cases actives de la colonne de droite',
+    'TEXT_HEADING_INACTIVE_LEFT_RIGHT' => 'Cases inactives de la page principale',
+    'TEXT_HEADING_HEADER_BOXES' => 'Cases d\'en-tête',
+    'TEXT_HEADING_FOOTER_BOXES' => 'Cases de pied de page',
+    'TEXT_HEADING_MOBILE_BOXES' => 'Cases de menu mobiles',
+    'TEXT_HEADING_ACTIVE_BOXES' => 'Cases actives',
+    'TEXT_HEADING_INACTIVE_BOXES' => 'Cases inactives',
+    'BUTTON_SHOW' => 'Afficher',
+    'BUTTON_HIDE' => 'Cacher',
+
+    'TEXT_MOVE_BOX_UP' => 'Déplacez %1$s vers le haut dans les cases %2$s.',
+    'TEXT_MOVE_BOX_DOWN' => 'Déplacez %1$s vers le bas dans les cases %2$s.',
+    'TEXT_MOVE_BOX_UNUSED' => 'Déplacez %1$s vers les cases %2$s inactives.',
+        'TEXT_MOVE_MAIN_PAGE_COLUMN' => 'page principale',  //- Used as %2$s value in above three phrases
+        'TEXT_MOVE_HEADER_COLUMN' => 'en-tête',        //- Used as %2$s value in the above three phrases
+        'TEXT_MOVE_FOOTER_COLUMN' => 'pied de page',        //- Used as %2$s value in the above three phrases
+        'TEXT_MOVE_MOBILE_COLUMN' => 'menu mobiles',   //- Used as %2$s value in the above three phrases
+    'BUTTON_SAVE_CHANGES' => 'Enregistrer les modifications',
     'SUCCESS_BOX_UPDATED' => 'Mise à jour réussie des paramètres du bloc : ',
-    'TEXT_ON' => ' ON ',
-    'TEXT_OFF' => ' OFF ',
-    'TEXT_LEFT' => ' GAUCHE ',
-    'TEXT_RIGHT' => ' DROITE ',
-    'TEXT_CAUTION_EDITING_NOT_LIVE_TEMPLATE' => 'ATTENTION : Vous modifiez les paramètres d\'un modèle qui n\'est pas le modèle principal utilisé par les clients.',
-    'TEXT_RESET_SETTINGS' => 'Réinitialiser les options',
-    'TEXT_ORIGINAL_DEFAULTS' => '[Paramètres par défaut d\'origine/maître de Zen-Cart]',
 ];
 
 return $define;
