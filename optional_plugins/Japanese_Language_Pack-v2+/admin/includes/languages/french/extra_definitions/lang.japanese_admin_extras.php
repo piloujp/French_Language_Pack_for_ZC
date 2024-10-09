9b167d54b3bc17d1edcdf65dff45710279fe28d7
piloujp <117885587+piloujp@users.noreply.github.com>
2024-10-09 22:10:31 +0900

    Reorganized atra constants

---
+++ optional_plugins/Japanese_Language_Pack-v2+/admin/includes/languages/french/extra_definitions/lang.japanese_admin_extras.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Zcwilt 2020 Jun 02 New in v1.5.8-alpha $
+*/
+
+$define = [
+    'TABLE_HEADING_FIRSTNAME_KANA' => 'Lecture du prénom',
+    'TABLE_HEADING_LASTNAME_KANA' => 'Lecture du nom',
+	'ADMIN_NAV_DATE_CALENDAR' => '',
+    'EMAIL_GREET' => '',
+    'ENTRY_FIRST_NAME_KANA' => 'Prénom (furigana) :',
+    'ENTRY_FIRST_NAME_KANA_ERROR' => '&nbsp;<span class="errorText">Votre prénom (furigana) est-il correct ? Notre système requiert un minimum de ' . ENTRY_FIRST_NAME_MIN_LENGTH . ' caractères. Veuillez réessayer.</span>',
+    'ENTRY_LAST_NAME_KANA' => 'Nom de famille (furigana):',
+    'ENTRY_LAST_NAME_KANA_ERROR' => '&nbsp;<span class="errorText">Votre nom (furigana) est-il correct ? Notre système requiert un minimum de ' . ENTRY_FIRST_NAME_MIN_LENGTH . ' caractères. Veuillez réessayer.</span>',
+    'ENTRY_NAME_KANA' => 'Prononciation: ',
+	'TEXT_TIME_SPECIFY' => 'Heure de livraison préférée :',
+];
+
+return $define;
