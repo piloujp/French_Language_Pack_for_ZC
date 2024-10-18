99c0df2014dca87a1c8b9aa65f965432fe51be46
piloujp <117885587+piloujp@users.noreply.github.com>
2024-10-19 02:18:38 +0900

    Added Boostrap admin translation and BFFM plugin translation

---
+++ optional_plugins/BFFM/v2.0.0/admin/includes/languages/french/extra_definitions/lang.BFFM.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: piloujp 2024 Oct 16 Modified in v2.1.0-beta1 $
+*/
+
+$define = [
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_BFFM' => 'Bootstrap : Menu de pied de page flexible et multilingue',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_BFFM' => 'Ce plugin permet à un administrateur Zen Cart de configurer un menu de pied de page multilingue et multi-colonnes pour le modèle Bootstrap.',
+];
+
+return $define;
---
+++ optional_plugins/BFFM/v2.0.0/admin/includes/languages/french/lang.flexible_footer_menu2.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * Flexible Footer Menu Multilingual (for Bootstrap)
+ *
+ * Last updated: v2.0.0
+ *
+ * @package admin
+ * English language defines
+ * @copyright Copyright 2003-2014 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ *
+ * @added for version 1.1 by Zen4All.nl (design75) 6-24-2015 $
+ *
+ */
+$define = [
+    'HEADING_TITLE' => 'Menu de pied de page flexible et multilingue II',
+
+    //Main Page Plugin Defines
+    'TABLE_HEADER_NAME' => 'Nom de l\'en-tête',
+    'TABLE_PAGE_NAME' => 'Nom de la page',
+    'TABLE_COLUMN_ID' => 'Colonne',
+    'TABLE_SORT_ORDER' => 'Ordre de tri',
+    'TABLE_STATUS' => 'Statut',
+    'TABLE_HEADER_LINK' => 'Lien',
+    'ICON_STATUS_GREEN' => 'Affichage dans le menu de pied de page',
+    'ICON_STATUS_RED' => 'Ne s\'affiche pas dans le menu de pied de page',
+    'BUTTON_DELETE_ALL' => 'Supprimer tout ?',
+    'BUTTON_NEW' => 'Ajouter une nouvelle page de colonne',
+    'SUCCESS_ALL_ITEMS_REMOVED' => 'Tous les éléments du menu de pied de page ont été supprimés.',
+    'SUCCESS_PAGE_STATUS_UPDATED' => 'Un statut de menu de pied de page a été mis à jour.',
+    'SUCCESS_PAGE_UPDATED' => 'Une entrée de menu de pied de page a été mise à jour.',
+
+    //New-Update Page Plugin Defines
+    'TEXT_COLUMN_HEADER' => 'En-tête/sous-en-tête de colonne :',
+    'TEXT_COLUMN_HEADER_TIP' => '(Option 1) Affiche le texte de l\'en-tête/sous-en-tête de colonne.',
+    'TEXT_PAGES_NAME' => 'Nom de la page :',
+    'TEXT_PAGES_NAME_TIP' => '(Option 2) Affiche le texte du nom des pages.',
+    'TEXT_HAS_IMAGE' => 'Image actuellement associée :',
+    'TEXT_USE_IMAGE' => 'Télécharger l\'image :',
+    'TEXT_USE_IMAGE_TIP' => '(Option 3) Utilisez une image pour afficher l\'en-tête/sous-en-tête de colonne ou le nom de la page au lieu du texte.',
+    'TEXT_INFO_DELETE_INTRO' => 'Êtes-vous sûr de vouloir supprimer cet élément de menu ?',
+    'TEXT_INFO_DELETE_ALL' => 'Cette action ne peut pas être annulée. Si vous avez besoin d\'une copie de ces éléments de menu, assurez-vous de disposer d\'une sauvegarde de base de données à jour avant de continuer !',
+    'TEXT_INFO_DELETE_ALL_INTRO' => 'Êtes-vous sûr de vouloir supprimer tous les éléments du menu ?',
+    'TEXT_LINKAGE' => 'Ajouter un lien　:',
+    'TEXT_LINKAGE_TIP' => 'Ajoutez une URL de lien à l\'en-tête/sous-en-tête de colonne, au nom de la page ou à l\'image.<br>Exemple d\'URL de lien INTERNE : index.php?main_page=reviews<br/>Exemple d\'URL de lien EXTERNE : https://www.zcadditions.com <b>(https://)</b> supprime l\'URL du lien INTERNE et l\'ouvre automatiquement dans un nouvel onglet/une nouvelle fenêtre.',
+    'TEXT_ADD_TEXT' => 'Texte de la colonne/Commentaires :',
+    'TEXT_ADD_TEXT_TIP' => 'Ajoutez du texte ou un commentaire sous l\'en-tête/sous-en-tête de colonne ou de nom de page.<br>Pour ajouter au-dessus de l\'en-tête/sous-en-tête de colonne ou du nom de la page, enlevez simplement 1 à l\'ordre de tri.<br>Vous pouvez même juste saisir votre texte ou votre commentaire et lui donner son propre ordre de tri.',
+    'TEXT_COLUMN' => 'Colonne :',
+    'TEXT_COLUMN_TIP' => 'Indiquez la colonne de pied de page dans laquelle réside cet élément.',
+    'TEXT_COLUMN_SORT' => 'Ordre de tri :',
+    'TEXT_COLUMN_SORT_TIP' => 'Indiquez l\'ordre de tri pour cet élément dans sa colonne respective.',
+    'FFM_TABLE_TITLE_HEADER' => 'En-tête',
+    'FFM_TABLE_TITLE_PAGE_NAME' => 'Nom de la page',
+    'FFM_TABLE_TITLE_IMAGE' => 'Image',
+];
+return $define;
