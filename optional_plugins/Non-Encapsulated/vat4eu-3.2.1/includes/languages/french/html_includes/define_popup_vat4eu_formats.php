257353702ad35b75cffa98873c22357d6b8cad4e
piloujp <117885587+piloujp@users.noreply.github.com>
2024-11-04 04:28:35 +0900

    Added VAT4EU plugin translation

---
+++ optional_plugins/Non-Encapsulated/vat4eu-3.2.1/includes/languages/french/html_includes/define_popup_vat4eu_formats.php
@@ -0,0 +1,165 @@
+<div class="p-2">
+    <h1>Structure du numéro d'identification de TVA</h1>
+    <p>Ces caractères sont utilisés dans la colonne <em>Structure</em> du tableau ci-dessous pour illustrer le format requis pour les différents <em>États membres</em> :
+    <ul>
+        <li>Le <em>format</em> exclut le préfixe alphabétique à 2 lettres</li>
+        <li><b>9</b>: Un chiffre</li>
+        <li><b>X</b>: Une lettre ou un chiffre</li>
+        <li><b>S</b>: Une lettre, un chiffre, <b>+</b> ou <b>*</b></li>
+        <li><b>L</b>: Une lettre</li>
+    </ul>
+
+    <table>
+        <tr>
+            <th>État membre</th>
+            <th>Structure</th>
+            <th>Format<sup>*</sup></th>
+        </tr>
+        <tr>
+            <td>AT-Autriche</td>
+            <td>ATU99999999</td>
+            <td>1 bloc de 9 caractères</td>
+        </tr>
+        <tr>
+            <td>BE-Belgique</td>
+            <td>BE0999999999</td>
+            <td>1 bloc de 10 chiffres</td>
+        </tr>
+        <tr>
+            <td>BG-Bulgarie</td>
+            <td>BG999999999 or<br>BG9999999999</td>
+            <td>1 bloc de 9 chiffres ou<br>1 bloc de 10 chiffres</td>
+        </tr>
+        <tr>
+            <td>CY-Chypre</td>
+            <td>CY99999999L</td>
+            <td>1 bloc de 9 caractères</td>
+        </tr>
+        <tr>
+            <td>CZ-République tchèque</td>
+            <td>CZ99999999 ou<br>CZ999999999 ou<br>CZ9999999999</td>
+            <td>1 bloc de 8, 9 ou 10 chiffres</td>
+        </tr>
+        <tr>
+            <td>DE-Allemagne</td>
+            <td>DE999999999</td>
+            <td>1 bloc de 9 chiffres</td>
+        </tr>
+        <tr>
+            <td>DK-Danemark</td>
+            <td>DK99 99 99 99</td>
+            <td>4 blocs de 2 chiffres</td>
+        </tr>
+        <tr>
+            <td>EE-Estonie</td>
+            <td>EE999999999</td>
+            <td>1 bloc de 9 chiffres</td>
+        </tr>
+        <tr>
+            <td>EL-Grèce</td>
+            <td>EL999999999</td>
+            <td>1 bloc de 9 chiffres</td>
+        </tr>
+        <tr>
+            <td>ES-Espagne</td>
+            <td>ESX9999999X<sup>4</sup></td>
+            <td>1 bloc de 9 caractères</td>
+        </tr>
+        <tr>
+            <td>FI-Finland</td>
+            <td>FI99999999</td>
+            <td>1 bloc de 8 chiffres</td>
+        </tr>
+        <tr>
+            <td>FR-France</td>
+            <td>FRXX 999999999</td>
+            <td>1 bloc de 2 caractères, 1 bloc de 9 chiffres</td>
+        </tr>
+        <tr>
+            <td>HR-Croatie</td>
+            <td>HR99999999999</td>
+            <td>1 bloc de 11 chiffres</td>
+        </tr>
+        <tr>
+            <td>HU-Hongrie</td>
+            <td>HU99999999</td>
+            <td>1 bloc de 8 chiffres</td>
+        </tr>
+        <tr>
+            <td>IE-Irlande</td>
+            <td>IE9S99999L<br>IE9999999WI</td>
+            <td>1 bloc de 8 caractères ou<br>1 bloc de 9 caractères</td>
+        </tr>
+        <tr>
+            <td>IT-Italie</td>
+            <td>IT99999999999</td>
+            <td>1 bloc de 11 chiffres</td>
+        </tr>
+        <tr>
+            <td>LT-Lituanie</td>
+            <td>LT999999999 ou<br>LT999999999999</td>
+            <td>1 bloc de 9 chiffres ou<br>1 bloc de 12 chiffres</td>
+        </tr>
+        <tr>
+            <td>LU-Luxembourg</td>
+            <td>LU99999999</td>
+            <td>1 bloc de 8 chiffres</td>
+        </tr>
+        <tr>
+            <td>LV-Lettonie</td>
+            <td>LV99999999999</td>
+            <td>1 bloc de 11 chiffres</td>
+        </tr>
+        <tr>
+            <td>MT-Malte</td>
+            <td>MT99999999</td>
+            <td>1 bloc de 8 chiffres</td>
+        </tr>
+        <tr>
+            <td>NL-Pays-Bas</td>
+            <td>NL999999999<b>B</b>99<sup>8</sup></td>
+            <td>1 bloc de 12 caractères</td>
+        </tr>
+        <tr>
+            <td>PL-Pologne</td>
+            <td>PL9999999999</td>
+            <td>1 bloc de 10 chiffres</td>
+        </tr>
+        <tr>
+            <td>PT-Portugal</td>
+            <td>PT999999999</td>
+            <td>1 bloc de 9 chiffres</td>
+        </tr>
+        <tr>
+            <td>RO-Roumanie</td>
+            <td>RO999999999</td>
+            <td>1 bloc de minimum 2 chiffres et maximum 10 chiffres</td>
+        </tr>
+        <tr>
+            <td>SE-Suède</td>
+            <td>SE999999999999</td>
+            <td>1 bloc de 12 chiffres</td>
+        </tr>
+        <tr>
+            <td>SI-Slovénie</td>
+            <td>SI99999999</td>
+            <td>1 bloc de 8 chiffres</td>
+        </tr>
+        <tr>
+            <td>SK-Slovaquie</td>
+            <td>SK9999999999</td>
+            <td>1 bloc de 10 chiffres</td>
+        </tr>
+    </table>
+    <p><strong>Remarques :</strong></p>
+    <ul id="vat4eu-notes">
+        <li><sup>1</sup> La 1ère position suivant le préfixe est toujours <b>U</b>.</li>
+        <li><sup>2</sup> Le premier chiffre suivant le préfixe est toujours zéro (<b>0</b>).</li>
+        <li><sup>3</sup> Le (nouveau) format à 10 chiffres est le résultat de l'ajout d'un zéro non significatif au (ancien) format à 9 chiffres.</li>
+        <li><sup>4</sup> Le premier et le dernier caractère peuvent être alphabétiques ou numériques, mais ils ne peuvent pas être tous les deux numériques.</li>
+        <li><sup>5</sup> Identifie les commerçants de succursales.</li>
+        <li><sup>6</sup> Identifie les ministères.</li>
+        <li><sup>7</sup> Identifie les autorités sanitaires.</li>
+        <li><sup>8</sup> La 10ème position suivant le préfixe est toujours <b>B</b>.</li>
+    </ul>
+</div>
---
+++ optional_plugins/Non-Encapsulated/vat4eu-3.2.1/includes/languages/french/modules/order_total/lang.ot_vat_reverse_charges.php
@@ -0,0 +1,14 @@
+<?php
+// -----
+// Part of the VAT4EU plugin by Cindy Merkin a.k.a. lat9 (cindy@vinosdefrutastropicales.com)
+// Copyright (c) 2017-2024 Vinos de Frutas Tropicales
+//
+// Last updated: v3.2.0
+//
+$define = [
+    'MODULE_ORDER_TOTAL_VAT_REVERSE_CHARGES_TITLE_ADMIN' => 'Affiche la notification d\'autoliquidation de la TVA',
+    'MODULE_ORDER_TOTAL_VAT_REVERSE_CHARGES_TITLE' => 'Soumis à l\'autoliquidation dans le pays de réception.',
+
+    'MODULE_ORDER_TOTAL_VAT_REVERSE_CHARGES_DESCRIPTION' => 'Fait partie du plugin <em>VAT4EU</em>. Lorsqu\'il est activé, il affiche une clause de non-responsabilité dans la commande lorsque cette dernière est éligible à un remboursement de la TVA.',
+];
+return $define;
