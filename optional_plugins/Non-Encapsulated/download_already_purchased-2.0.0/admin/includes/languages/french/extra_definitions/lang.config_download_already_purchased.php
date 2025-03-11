df20cedba7426205c759a005c826354c34c4a33d
piloujp <117885587+piloujp@users.noreply.github.com>
2025-03-11 10:43:23 +0900

    Added translation for plugin Download Already Purchased v2.0.0

---
+++ optional_plugins/Non-Encapsulated/download_already_purchased-2.0.0/admin/includes/languages/french/extra_definitions/lang.config_download_already_purchased.php
@@ -0,0 +1,12 @@
+<?php
+
+$define = [
+    'CFGTITLE_DOWNLOAD_ALREADY_PURCHASED_MESSAGING' => 'Téléchargement déjà acheté : messagerie',
+    'CFGDESC_DOWNLOAD_ALREADY_PURCHASED_MESSAGING' => '<br>Choisissez la méthode avec laquelle communiquer l\'achat antérieur du téléchargement d\'un produit au client, l\'une des suivantes :<ol><li><strong>Désactivé :</strong> aucune manipulation particulière ; le téléchargement peut toujours être racheté sans message.</li><li><strong>Appel à l\'expiration :</strong> quel que soit le statut d\'expiration d\'un téléchargement de produit, l\'article ne peut pas être racheté. Si le téléchargement n\'a pas expiré, le client est dirigé vers la page d\'informations de commande, où le lien de téléchargement actif est affiché. Sinon, un message s\'affiche indiquant que le client a déjà acheté le produit et doit contacter la boutique pour obtenir la réinitialisation du téléchargement.</li><li><strong>Appliquer l\'expiration :</strong> si le téléchargement n\'a pas expiré, l\'article ne peut pas être racheté. Le client est, à la place, dirigé vers la page d\'informations de commande où le lien de téléchargement actif est affiché. Sinon, le client peut racheter le produit.</li></ol>',
+    'CFGTITLE_DOWNLOAD_ALREADY_PURCHASED_EXCLUDE_PRODUCTS' => 'Télécharger les produits déjà achetés : Exclusions de produits',
+    'CFGDESC_DOWNLOAD_ALREADY_PURCHASED_EXCLUDE_PRODUCTS' => '<br>Saisissez la liste séparée par des virgules des valeurs d\'ID de produit à exclure du traitement de « déjà achetés ».',
+    'CFGTITLE_DOWNLOAD_ALREADY_PURCHASED_EXCLUDE_CATEGORIES' => 'Télécharger Déjà acheté : Exclusions de catégorie',
+    'CFGDESC_DOWNLOAD_ALREADY_PURCHASED_EXCLUDE_CATEGORIES' => '<br>Saisissez la liste séparée par des virgules des valeurs d\'ID de catégorie pour lesquelles les produits associés doivent être exclus du traitement de « déjà achetés ».',
+];
+
+return $define;
---
+++ optional_plugins/Non-Encapsulated/download_already_purchased-2.0.0/includes/languages/french/extra_definitions/lang.download_already_purchased_messages.php
@@ -0,0 +1,27 @@
+<?php
+// -----
+// Part of the "Download Already Purchased" plugin created by lat9.
+// Copyright (C) 2017-2025, Vinos de Frutas Tropicales
+//
+// NOTE:  These messages make use of the PHP variable-ordering (to make translations easier); make sure that you ARE NOT
+// enclosing the message text using double-quotes ("), since the $ in the variable name will be improperly interpreted
+// as a PHP variable!
+//
+
+// -----
+// This messages, issued by the plugin's "extra_cart_action" and "observer" processing, lets the customer know that they've got an
+// active download link, so they don't need to re-purchase.  For each message, there's one form used by the cart-action and one
+// used by the observer; the following "sprintf" variables are used in these messages:
+//
+// %1$s ... The name of the download product, as retrieved from the orders_products table.
+// %2$s ... Either a link to the associated account_history_info page to reference the associated customer order or a link to the contact_us page.
+//
+return [
+    'DAP_MESSAGE_DOWNLOAD_AVAILABLE_NOT_ADDED' => 'Vous avez un téléchargement actif disponible pour <em>%1$s</em> ! Cliquez <a href="%2$s">ici</a> pour accéder à ce téléchargement ; le produit n\'a pas été ajouté à votre panier.',
+
+    'DAP_MESSAGE_DOWNLOAD_EXPIRED_CALL_US_NOT_ADDED' => 'Vous avez déjà acheté <em>%1$s</em>, mais votre lien de téléchargement a actuellement expiré. <a href="%2$s">Contactez-nous</a> et nous réactiverons ce téléchargement ; le produit n\'a pas été ajouté à votre panier.',
+
+    'DAP_MESSAGE_DOWNLOAD_AVAILABLE_REMOVED' => 'Vous avez un téléchargement actif disponible pour <em>%1$s</em> ! Cliquez <a href="%2$s">ici</a> pour accéder à ce téléchargement ; le produit a été supprimé de votre panier.',
+
+    'DAP_MESSAGE_DOWNLOAD_EXPIRED_CALL_US_REMOVED', 'Vous avez déjà acheté <em>%1$s</em>, mais votre lien de téléchargement a actuellement expiré. <a href="%2$s">Contactez-nous</a> et nous réactiverons ce téléchargement ; le produit a été supprimé de votre panier.',
+];
