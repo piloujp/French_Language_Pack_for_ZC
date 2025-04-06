fe777f05a3b7d7b84460afc20baf8539d836b8b2
piloujp <117885587+piloujp@users.noreply.github.com>
2025-04-07 04:36:47 +0900

    Added Languages-Currencies links in header plugin translation

---
+++ optional_plugins/Non-Encapsulated/LanguageCurrency_Links_in_Header_v4/admin/includes/languages/french/extra_definitions/lang.config_language_currency_header.php
@@ -0,0 +1,10 @@
+<?php
+
+$define = [
+    'CFGTITLE_HEADER_LANGUAGES_DISPLAY' => 'Afficher les langues dans l\'en-tête ?',
+    'CFGDESC_HEADER_LANGUAGES_DISPLAY' => 'Afficher les drapeaux/liens des langues dans l\'en-tête ?',
+    'CFGTITLE_HEADER_CURRENCIES_DISPLAY' => 'Afficher les devises dans l\'en-tête ?',
+    'CFGDESC_HEADER_CURRENCIES_DISPLAY' => 'Display the Currencies symbols/links in Header?',
+];
+
+return $define;
