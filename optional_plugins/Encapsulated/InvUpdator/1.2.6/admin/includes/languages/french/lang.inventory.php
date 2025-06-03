974a2cb045601e66a983a3bb63a831acf631c4cd
piloujp <117885587+piloujp@users.noreply.github.com>
2025-06-03 23:38:54 +0900

    Added plugin Inventory updater translation

---
+++ optional_plugins/Encapsulated/InvUpdator/1.2.6/admin/includes/languages/french/lang.inventory.php
@@ -0,0 +1,41 @@
+<?php
+    /**
+    *  developed, copyrighted and brought to you by @proseLA (github)
+    *  https://mxworks.cc
+    *  copyright 2025 proseLA
+    *
+    *  consider a donation.
+    *
+    *  released under GPU
+    *  https://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+    *
+    *  use of this software constitutes acceptance of license
+    *  mxworks will vigilantly pursue any violations of this license.
+    *
+    *  some portions of code may be copyrighted and licensed by www.zen-cart.com
+    *
+    *  05/2025  project: inventory v1.2.6 file: lang.inventory.php
+    */
+
+
+    $define = [
+        'INVENTORY_PAGE' => 'Maintenance des stocks',
+        'TABLE_HEADING_ID' => 'ID',
+        'TABLE_HEADING_MODEL' => 'Modèle',
+        'TABLE_HEADING_NAME' => 'Nom du produit',
+        'TABLE_HEADING_STATUS' => 'Statut',
+        'TABLE_HEADING_QTY' => 'Quantité',
+        'TABLE_HEADING_DISPLAY_PRICE' => 'Afficher le prix',
+        'TABLE_HEADING_BASE_PRICE' => 'Prix ​​de base',
+
+        'ACTIVE_STATUS' => 'Actif/Inactif : ',
+        'CATEGORY_SELECTOR' => 'Catégorie : ',
+
+        'PRODUCTS_UPDATED' => ' inventaire des produits mis à jour.',
+        'PRODUCTS_PRICE_UPDATED' => ' prix de base du/des produit(s) mis à jour.',
+
+        'BUTTON_UPDATE' => 'mettre à jour les éléments',
+        'BUTTON_RESET' => 'réinitialiser',
+
+    ];
+    return $define;
