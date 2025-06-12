20bc284ff5c6905823789aa6de7505bfc51fce89
piloujp <117885587+piloujp@users.noreply.github.com>
2025-06-12 15:38:49 +0900

    Added offsite payment translation

---
+++ optional_plugins/Non-Encapsulated/offsitepayment-v3/includes/languages/french/modules/payment/lang.offsitepayment.php
@@ -0,0 +1,18 @@
+<?php
+
+$define = [
+    'MODULE_PAYMENT_OFFSITEPAYMENT_TEXT_TITLE' => 'Facturez-moi / Payer plus tard',
+    'MODULE_PAYMENT_OFFSITEPAYMENT_TEXT_DESCRIPTION' => '',
+    'MODULE_PAYMENT_OFFSITEPAYMENT_TEXT_EMAIL_FOOTER' => '', // 'Nous vous enverrons par e-mail une demande de paiement pour le total de la commande.'
+// Beginning of constant configuration titles and descriptions for payment module offsitepayment
+    'CFGTITLE_MODULE_PAYMENT_OFFSITEPAYMENT_STATUS' => 'Activer le module de paiement hors site',
+    'CFGDESC_MODULE_PAYMENT_OFFSITEPAYMENT_STATUS' => 'Souhaitez-vous accepter les paiements hors site ?',
+    'CFGTITLE_MODULE_PAYMENT_OFFSITEPAYMENT_SORT_ORDER' => 'Ordre de tri d\'affichage.',
+    'CFGDESC_MODULE_PAYMENT_OFFSITEPAYMENT_SORT_ORDER' => 'Ordre d\'affichage : le plus bas est affiché en premier.',
+    'CFGTITLE_MODULE_PAYMENT_OFFSITEPAYMENT_ZONE' => 'Zone de paiement',
+    'CFGDESC_MODULE_PAYMENT_OFFSITEPAYMENT_ZONE' => 'Si une zone est sélectionnée, activez uniquement ce mode de paiement pour cette zone.',
+    'CFGTITLE_MODULE_PAYMENT_OFFSITEPAYMENT_ORDER_STATUS_ID' => 'Définir le statut de la commande',
+    'CFGDESC_MODULE_PAYMENT_OFFSITEPAYMENT_ORDER_STATUS_ID' => 'Définissez le statut des commandes effectuées avec ce module de paiement sur cette valeur',
+];
+
+return $define;
