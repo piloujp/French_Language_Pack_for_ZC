5a05b41f878e4772f9332cba8bfe82d6101af962
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-21 23:23:55 +0900

    Installer and SQL update for ZC v2.2.0

--- Files_to_Install/includes/languages/french/classic/lang.header.php
+++ Files_to_Install/includes/languages/french/classic/lang.header.php
@@ -1,19 +1,8 @@
 <?php
-$define = [
-    'HEADER_TITLE_MY_ACCOUNT' => 'Mon compte',
-    'HEADER_TITLE_CART_CONTENTS' => 'Panier',
-    'HEADER_TITLE_CHECKOUT' => 'Encaisser',
-    'HEADER_TITLE_CATALOG' => 'Accueil',
-    'HEADER_TITLE_LOGOFF' => 'Déconnection',
-    'HEADER_TITLE_LOGIN' => 'Connection',
-    'HEADER_ALT_TEXT' => 'Powered by Zen Cart :: The Art of E-Commerce',
-    'HEADER_SALES_TEXT' => '<h1>Le message de vente va ici</h1>',
-    'HEADER_LOGO_WIDTH' => '100%',
-    'HEADER_LOGO_HEIGHT' => 'auto',
-    'HEADER_LOGO_IMAGE' => 'logo.gif',
-    'HEADER_SEARCH_BUTTON' => 'Rechercher',
-    'HEADER_SEARCH_DEFAULT_TEXT' => 'Entrer les mots-clefs de recherche ici',
-    'SEARCH_DEFAULT_TEXT' => 'Recherchez ici',
-];
+    $define = [
+        'HEADER_SALES_TEXT' => '<h1>Le message de vente va ici</h1>',
+        'HEADER_LOGO_WIDTH' => '100%',
+        'HEADER_LOGO_HEIGHT' => 'auto',
+    ];
 
-return $define;
+    return $define;
--- sql/oders_status_update.sql
+++ sql/oders_status_update.sql
@@ -1,16 +1,16 @@
-SET @lgid=(SELECT languages_id FROM languages WHERE code = 'fr' LIMIT 1);
+SET @lan_id=(SELECT languages_id FROM languages WHERE code = 'fr' LIMIT 1);
 
-UPDATE orders_status SET orders_status_name='En attente', sort_order=0 WHERE language_id=@lgid AND orders_status_name='Pending';
-UPDATE orders_status SET orders_status_name='En cours', sort_order=10 WHERE language_id=@lgid AND orders_status_name='Processing';
-UPDATE orders_status SET orders_status_name='Livré', sort_order=20 WHERE language_id=@lgid AND orders_status_name='Delivered';
-UPDATE orders_status SET orders_status_name='Mise à jour', sort_order=30 WHERE language_id=@lgid AND orders_status_name='Update';
-UPDATE orders_status SET orders_status_name='Envoyé', sort_order=15 WHERE language_id=@lgid AND orders_status_name='Sent';
+UPDATE orders_status SET orders_status_name='En attente', sort_order=0 WHERE language_id=@lan_id AND orders_status_name='Pending';
+UPDATE orders_status SET orders_status_name='En cours', sort_order=10 WHERE language_id=@lan_id AND orders_status_name='Processing';
+UPDATE orders_status SET orders_status_name='Livré', sort_order=20 WHERE language_id=@lan_id AND orders_status_name='Delivered';
+UPDATE orders_status SET orders_status_name='Mise à jour', sort_order=30 WHERE language_id=@lan_id AND orders_status_name='Update';
+UPDATE orders_status SET orders_status_name='Envoyé', sort_order=15 WHERE language_id=@lan_id AND orders_status_name='Sent';
 
 UPDATE layout_boxes SET layout_box_status=1, layout_box_sort_order=0 WHERE layout_box_name = 'languages.php';
 
 # if POSM is installed
-SET @tbl_exists = (SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = DATABASE() AND table_name = 'products_options_stock_names');
-SET @sql = IF(@tbl_exists = 0,'SELECT ""','UPDATE IGNORE products_options_stock_names SET pos_name="En rupture de stock" WHERE language_id=@lgid AND pos_name_id=1');
-PREPARE stmt FROM @sql;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
+SET @tbl_posm_exists = (SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = DATABASE() AND table_name = 'products_options_stock_names');
+SET @sql_posm = IF(@tbl_posm_exists = 0,'SELECT 1','UPDATE IGNORE products_options_stock_names SET pos_name="En rupture de stock" WHERE language_id=@lan_id AND pos_name_id=1');
+PREPARE stmt_posm FROM @sql_posm;
+EXECUTE stmt_posm;
+DEALLOCATE PREPARE stmt_posm;
