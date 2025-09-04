2dbfa59b1a4cbbe06aba4de1655d5e8c1ff3b832
piloujp <117885587+piloujp@users.noreply.github.com>
2025-09-05 01:52:02 +0900

    Added translation for PayPayl RESTful plugin v1.2.0

---
+++ optional_plugins/Non-Encapsulated/PayPal_RESTful/paypalr-1.2.0/includes/languages/french/extra_definitions/lang.paypalr_redirect_listener_definitions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Language definitions for the paypalr (PayPal Restful Api) payment module's
+ * /ppr_listener.php
+ *
+ * Last updated: v1.2.0
+ */
+return [
+    'MODULE_PAYMENT_PAYPALR_REDIRECT_LISTENER_TRY_AGAIN' => 'Veuillez réessayer, sélectionnez un autre mode de paiement ou contactez-nous pour obtenir de l\'aide.',
+];
