46eb318ee355abe3c063d17c9cdb54781b5a17b4
piloujp <117885587+piloujp@users.noreply.github.com>
2025-09-10 10:25:10 +0900

    Update lang.password_forgotten.php

--- Files_to_Install/includes/languages/french/lang.password_forgotten.php
+++ Files_to_Install/includes/languages/french/lang.password_forgotten.php
@@ -5,7 +5,7 @@ $define = [
     'HEADING_TITLE' => 'Mot de passe oublié',
     'TEXT_MAIN' => 'Saisissez votre adresse e-mail ci-dessous et nous vous enverrons des instructions pour réinitialiser votre mot de passe.',
     'EMAIL_PASSWORD_RESET_SUBJECT' => STORE_NAME . ' - Réinitialisation du mot de passe',
-    'EMAIL_PASSWORD_RESET_BODY' => "Une réinitialisation du mot de passe a été demandée par %1s. \n\nPour réinitialiser votre mot de passe pour '%2s', veuillez visiter l'URL suivante :\n\n%3s \n\n",
+    'EMAIL_PASSWORD_RESET_BODY' => "Une réinitialisation du mot de passe a été demandée par %1\$s.\n\nPour réinitialiser votre mot de passe pour '%2\$s', veuillez visiter l'URL suivante :\n\n%3\$s\n\n",
     'SUCCESS_PASSWORD_RESET_SENT' => 'Merci. Si cette adresse e-mail se trouve dans notre système, nous l\'utiliserons pour envoyer des instructions de récupération de mot de passe (n\'oubliez pas de vérifier votre dossier Spam).',
 ];
 
