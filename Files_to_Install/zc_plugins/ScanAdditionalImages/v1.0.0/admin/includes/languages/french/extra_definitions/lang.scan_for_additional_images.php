3fbe519dca120fd6553d2d7178b817b069778e5a
piloujp <117885587+piloujp@users.noreply.github.com>
2025-10-04 03:29:59 +0900

    Scan Additional Images plugin language files update

--- Files_to_Install/zc_plugins/ScanAdditionalImages/v1.0.0/admin/includes/languages/french/extra_definitions/lang.scan_for_additional_images.php
+++ Files_to_Install/zc_plugins/ScanAdditionalImages/v1.0.0/admin/includes/languages/french/extra_definitions/lang.scan_for_additional_images.php
@@ -2,6 +2,9 @@
 
 $define = [
     'BOX_TOOLS_SCAN_FOR_IMAGES' => 'Numériser des images de produits supplémentaires dans la base de données',
+
+    'SYNC_TEXT_NOTHING_TO_PROCESS' => 'Aucun produit (avec images) à traiter avec ces sélections par lots. Vérifiez les paramètres « Démarrer à » et « Taille du lot ».',
+    'SYNC_TEXT_QUERY_ONLY' => 'Requête uniquement - aucun traitement effectué.',
 ];
 
 return $define;
