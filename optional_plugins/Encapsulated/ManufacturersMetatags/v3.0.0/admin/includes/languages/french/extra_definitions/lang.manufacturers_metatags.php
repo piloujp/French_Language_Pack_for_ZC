10362269c9089afcf729d74c6107cb7f2f89cb9d
piloujp <117885587+piloujp@users.noreply.github.com>
2025-10-30 10:52:57 +0900

    Added Manufacturers Metatags plugin's translation

---
+++ optional_plugins/Encapsulated/ManufacturersMetatags/v3.0.0/admin/includes/languages/french/extra_definitions/lang.manufacturers_metatags.php
@@ -0,0 +1,9 @@
+<?php
+// -----
+// Part of the Manufacturers Meta Tags plugin for Zen Cart.
+//
+return [
+    'TEXT_EDIT_MANUFACTURER_META_TAGS_TITLE' => 'Titre des métadonnées :',
+    'TEXT_EDIT_MANUFACTURER_META_TAGS_KEYWORDS' => 'Mots clés des métadonnées :',
+    'TEXT_EDIT_MANUFACTURERS_META_TAGS_DESCRIPTION' => 'Description des métadonnées :',
+];
---
+++ optional_plugins/Encapsulated/ManufacturersMetatags/v3.0.0/admin/includes/languages/french/extra_definitions/lang.menu.php
@@ -0,0 +1,8 @@
+<?php
+
+$define = [
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_MANUFACTURERSMETATAGS' => 'Métadonnées des fabricants',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_MANUFACTURERSMETATAGS' => 'Ce plugin permet à votre boutique d’attribuer un titre, un mot-clé et/ou un texte de description de balise méta spécifiques au fabricant, de manière similaire au traitement des balises méta spécifiques aux produits intégré à Zen Cart.',
+];
+
+return $define;
