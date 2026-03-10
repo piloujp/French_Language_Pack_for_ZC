7da185adc983252f74784f93f4006370acdcaa4e
piloujp <117885587+piloujp@users.noreply.github.com>
2026-03-11 00:08:36 +0900

    Update to ZCA Bootstrap temple v3.7.8

---
+++ optional_plugins/Non-Encapsulated/ZCA-Bootstrap-Template-3.7.8/admin/includes/languages/french/lang.zca_bootstrap_colors.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ *  zca_bootstrap_colors.php
+ *
+ * @copyright Copyright 2003-2018 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license https://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version Author: rbarbour (ZCAdditions.com)  7/01/2018 06:00 AM Modified ZCA-BS-COLORS
+ *
+ * BOOTSTRAP v3.7.9
+ */
+return [
+    'HEADING_TITLE' => 'Couleurs de ZCA Bootstrap',
+
+    'TEXT_INFORMATION' => 'Toutes les couleurs ajoutées pour la version 3.5.2 du modèle et les versions ultérieures sont initialisées lors d’une mise à niveau vers « non défini », permettant à un magasin de définir ces valeurs pour qu’elles correspondent à sa palette de couleurs avant utilisation. <b>Seules</b> les couleurs indiquant que « non défini » est une valeur acceptable doivent utiliser cette valeur; sinon, le script de la vitrine qui crée la coloration CSS produira un CSS non valide !<br><br><b>Remarque :</b> la colonne <em>Valeur</em> sera vide si la valeur actuellement configurée est la valeur par défaut.',
+
+    'TEXT_INFO_EDIT_INTRO' => 'Veuillez apporter les modifications nécessaires',
+    'TEXT_INFO_DATE_ADDED' => 'Date d’ajout :',
+    'TEXT_INFO_LAST_MODIFIED' => 'Dernière modification :',
+
+    'TABLE_HEADING_CONFIGURATION_TITLE' => 'Titre',
+    'TABLE_HEADING_CONFIGURATION_DEFAULT' => 'Par défaut',
+    'TABLE_HEADING_CONFIGURATION_VALUE' => 'Valeur',
+    'TABLE_HEADING_NOT_SET_OK' => '"non défini" OK ?',
+    'TABLE_HEADING_ACTION' => 'Action',
+
+    // BOF SQL file
+    'BUTTON_DOWNLOAD_SQL' => 'Télécharger SQL',
+    // EOF SQL file
+
+    // BOF CSV file
+    'BUTTON_DOWNLOAD_CSV' => 'Télécharger CSV',
+    'BUTTON_UPLOAD_CSV' => 'Télécharger CSV',
+    'TEXT_QUERY_FILENAME' => 'Télécharger le fichier :',
+
+    'CSV_HEADER_KEY' => 'Clé',
+    'CSV_HEADER_VALUE' => 'Valeur',
+    'CSV_HEADER_TITLE' => 'Titre',
+    'CSV_HEADER_DEFAULT' => 'Par défaut',
+
+    'UPLOAD_FILE_PROCESSED_ALL_OK' => "Fichier traité; toutes les %s mises à jour ont réussi.",
+    'UPLOAD_FILE_PROCESSED_SOME_OK' => "Fichier traité; %s sur %s mises à jour réussies.",
+
+    'UPLOAD_SUCCESS' => 'Succès : ',
+    'UPLOAD_WARNING' => 'Avertissement : ',
+    'UPLOAD_FAILED' => 'Échoué : ',
+    'MISSING_CONFIGURATION' => 'Configuration des couleurs ZCA Bootstrap non trouvée.',
+    'NO_CSV_FILE' => 'Fichier CSV non spécifié ou vide.',
+    'CSV_FILE_MALFORMED' => 'Fichier CSV mal formé.',
+    // EOF CSV file
+];
