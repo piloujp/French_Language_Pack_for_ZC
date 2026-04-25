705e9f1f76a8a128702227b8437f0cfcf1c3c910
piloujp <117885587+piloujp@users.noreply.github.com>
2026-04-26 04:23:39 +0900

    plugins update and addition
    Access Blocker v2.0.3
    Ez Pages Meta-tags v5.0.0

---
+++ optional_plugins/Encapsulated/EzpagesMetatags/5.0.0/Installer/languages/french/main.php
@@ -0,0 +1,2 @@
+<?php
+zen_define_default('ERROR_UNABLE_TO_DELETE_FILE', 'Impossible de supprimer l’ancien fichier : %s');
---
+++ optional_plugins/Encapsulated/EzpagesMetatags/5.0.0/admin/includes/languages/french/extra_definitions/lang.menu.php
@@ -0,0 +1,7 @@
+<?php
+$define = [
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_EZPAGESMETATAGS' => 'Métadonnées EZ Pages',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_EZPAGESMETATAGS' => 'Permet la saisie de balises méta personnalisées pour EZ Pages.',
+];
+
+return $define;
