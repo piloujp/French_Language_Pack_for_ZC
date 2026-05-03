3a8c406f0799d593342b97afd0373cc24cae1d52
piloujp <117885587+piloujp@users.noreply.github.com>
2026-05-04 03:58:13 +0900

    TyPackage tracker plugin update to v5.0.2

---
+++ optional_plugins/Encapsulated/PackageTracker/v5.0.2/catalog/includes/languages/french/extra_definitions/lang.ty_package_tracker_definitions.php
@@ -0,0 +1,20 @@
+<?php
+// -----
+// Part of the Ty Package Tracker plugin, v4.0.0 and later.
+//
+// Last updated: v5.0.0
+//
+$define = [
+    // -----
+    // Definition, used for account_history_info page's status-table column heading.
+    //
+    'TABLE_HEADING_TRACKING_ID' => 'ID de suivi',
+
+    // -----
+    // Identify constants associated with the "Track Orders" sidebox.
+    //
+    'BOX_HEADING_TRACK_ORDERS' => 'Suivre les commandes',
+    'TRACK_ORDERS_ORDERNUM' => 'Commande #',
+    'TRACK_ORDERS_DATE' => ', commande passée le : ',
+];
+return $define;
