f2bc32ec93b1abb1d95b12aae3f6e4c27b6badc6
piloujp <117885587+piloujp@users.noreply.github.com>
2026-05-05 02:24:14 +0900

    POSM plugin update to v6.1.2
    + Japanese Language Pack files update

---
+++ optional_plugins/Non-Encapsulated/Japanese_Language_Pack-v3.0.0/admin/includes/languages/french/extra_definitions/lang.japanese_admin_extras.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @copyright Copyright 2003-2025 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: pilouJP 2025 Oct 13 v2.2.0-alpha1 $
+*/
+
+$define = [
+    'TABLE_HEADING_FIRSTNAME_KANA' => 'Lecture du prénom',
+    'TABLE_HEADING_LASTNAME_KANA' => 'Lecture du nom',
+    'ADMIN_NAV_DATE_CALENDAR' => '',
+    'EMAIL_GREET' => '',
+    'ENTRY_FIRST_NAME_KANA' => 'Prénom (furigana) :',
+    'ENTRY_FIRST_NAME_KANA_ERROR' => '&nbsp;<span class="errorText">Votre prénom (furigana) est-il correct ? Notre système requiert un minimum de ' . ENTRY_FIRST_NAME_MIN_LENGTH . ' caractères. Veuillez réessayer.</span>',
+    'ENTRY_LAST_NAME_KANA' => 'Nom de famille (furigana):',
+    'ENTRY_LAST_NAME_KANA_ERROR' => '&nbsp;<span class="errorText">Votre nom (furigana) est-il correct ? Notre système requiert un minimum de ' . ENTRY_FIRST_NAME_MIN_LENGTH . ' caractères. Veuillez réessayer.</span>',
+    'ENTRY_NAME_KANA' => 'Prononciation: ',
+    'TEXT_TIME_SPECIFY' => 'Heure de livraison préférée :',
+    'ADDRESS_SALUTATION' => '様',
+];
+
+return $define;
---
+++ optional_plugins/Non-Encapsulated/Japanese_Language_Pack-v3.0.0/includes/languages/french/extra_definitions/lang.japanese_extras.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: piloujp 2024 Oct 14 in v2.1.0-beta1 $
+*/
+
+$define = [
+    'EMAIL_GREET' => '',
+    'ENTRY_FIRST_NAME_KANA' => 'Prénom (furigana) :',
+    'ENTRY_FIRST_NAME_KANA_ERROR' => '&nbsp;<span class="errorText">Votre prénom (furigana) est-il correct ? Notre système requiert un minimum de ' . ENTRY_FIRST_NAME_MIN_LENGTH . ' caractères. Veuillez réessayer.</span>',
+    'ENTRY_FIRST_NAME_KANA_TEXT' => '',
+    'ENTRY_LAST_NAME_KANA' => 'Nom (furigana) :',
+    'ENTRY_LAST_NAME_KANA_ERROR' => '&nbsp;<span class="errorText">Votre nom (furigana) est-il correct ? Notre système requiert un minimum de ' . ENTRY_LAST_NAME_MIN_LENGTH . ' caractères. Veuillez réessayer.</span>',
+    'ENTRY_LAST_NAME_KANA_TEXT' => '',
+    'TEXT_TIME_SPECIFY' => 'Heure de livraison préférée : ',
+    'ADDRESS_SALUTATION' => '様',
+];
+
+return $define;
