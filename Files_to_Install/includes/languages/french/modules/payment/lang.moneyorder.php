534c72e55f5a788c92655bd2b5271740d600597c
piloujp <117885587+piloujp@users.noreply.github.com>
2026-05-24 05:37:09 +0900

    Update to lastest ZC files (zen_config)

--- Files_to_Install/includes/languages/french/modules/payment/lang.moneyorder.php
+++ Files_to_Install/includes/languages/french/modules/payment/lang.moneyorder.php
@@ -1,7 +1,7 @@
 <?php
 $define = [
     'MODULE_PAYMENT_MONEYORDER_TEXT_TITLE' => 'Chèque/Mandat postal',
-    'MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION' => 'Les clients peuvent envoyer leur paiement par courrier. Leur e-mail de confirmation de commande leur demandera de : <br><br>Veuillez établir votre chèque ou virement bancaire à l’ordre de :<br>' . (defined('MODULE_PAYMENT_MONEYORDER_PAYTO') ? MODULE_PAYMENT_MONEYORDER_PAYTO : '<br>(le nom de votre magasin)') . '<br><br>Envoyez votre paiement à :<br>' . nl2br(STORE_NAME_ADDRESS) . '<br><br>' . 'Votre commande ne sera pas expédiée tant que nous n’aurons pas reçu le paiement.',
+    'MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION' => 'Les clients peuvent envoyer leur paiement par courrier. Leur e-mail de confirmation de commande leur demandera de : <br><br>Veuillez établir votre chèque ou virement bancaire à l’ordre de :<br>' . (defined('MODULE_PAYMENT_MONEYORDER_PAYTO') ? zen_config('MODULE_PAYMENT_MONEYORDER_PAYTO') : '<br>(le nom de votre magasin)') . '<br><br>Envoyez votre paiement à :<br>' . nl2br(zen_config('STORE_NAME_ADDRESS')) . '<br><br>' . 'Votre commande ne sera pas expédiée tant que nous n’aurons pas reçu le paiement.',
     'MODULE_PAYMENT_MONEYORDER_REMINDER' => 'Veuillez vous assurer d’inscrire votre numéro de commande sur le recto de votre chèque ou mandat-postal.',
 // bof constant configuration titles and descriptions for payment module moneyorder
     'CFGTITLE_MODULE_PAYMENT_MONEYORDER_STATUS' => 'Activer le module « Chèque/Mandat postal »',
@@ -17,7 +17,7 @@ $define = [
 // eof constant configuration titles and descriptions for payment module moneyorder
 ];
 if (defined('MODULE_PAYMENT_MONEYORDER_STATUS')) {
-    $define['MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER'] = 'Veuillez établir votre chèque ou virement bancaire à l’ordre de :' . "\n\n" . MODULE_PAYMENT_MONEYORDER_PAYTO . "\n\n" . 'Envoyez votre paiement à :' . "\n" . STORE_NAME_ADDRESS . "\n\n" . 'Votre commande ne sera envoyée qu’à réception du règlement et de sa validation par notre banque.';
+    $define['MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER'] = 'Veuillez établir votre chèque ou virement bancaire à l’ordre de :' . "\n\n" . zen_config('MODULE_PAYMENT_MONEYORDER_PAYTO') . "\n\n" . 'Envoyez votre paiement à :' . "\n" . zen_config('STORE_NAME_ADDRESS') . "\n\n" . 'Votre commande ne sera envoyée qu’à réception du règlement et de sa validation par notre banque.';
 }
 
 return $define;
