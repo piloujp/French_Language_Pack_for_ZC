6648d94f0bff32f9e42c05de0f051de3df2eca62
piloujp <117885587+piloujp@users.noreply.github.com>
2026-07-22 04:33:42 +0900

    Update plugin Edit Order to version 5.0.4

---
+++ optional_plugins/Encapsulated/EditOrders/v5.0.4/catalog/includes/languages/french/modules/order_total/lang.ot_misc_cost.php
@@ -0,0 +1,11 @@
+<?php
+// -----
+// Gather any Miscellaneous Cost for an order, an order-total created by lat9 (https://vinosdefrutastropicales.com)
+//
+// Last updated: EO 5.0.0
+//
+$define = [
+    'MODULE_ORDER_TOTAL_MISC_COST_TITLE' => 'Frais divers',
+    'MODULE_ORDER_TOTAL_MISC_COST_DESCRIPTION' => 'Un coût divers associé à la commande.',
+];
+return $define;
